e97cd13432782932b99fa7fc5d23792de265fcee
mbugu <mwangis@cesbio.cnes.fr>
2023-08-18 09:31:27 +0200

    .

--- src/py/sparse.py
+++ src/py/sparse.py
@@ -31,12 +31,12 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
     albsmn          = 0.1;  albsmx= 0.5;                                                                # maybe shld not be hardcoded ??
 
     # partial pressure [ea], apparent emissivity [emisa], and sky radiance [ratm]
-    ea		    = 0.01*rh*6.11*(math.e**(17.269*(Ta-273.15)/(Ta-35.85)))
+    ea		    = 0.01*rh*6.11*(math.e**(17.269*(Ta - 273.15)/(Ta - 35.85)))
     emisa	    = 1.24*((ea/Ta)**(1/7))
     ratm	    = emisa*sigma*(Ta**4)
     # related terms appearing in the [Taylor] SEB linearization
-    da		    = 6.11*(math.e**(17.269*(Ta-273.15)/(Ta-35.85))) - ea
-    delta	    = ((da+ea)*4097.9337)*((Ta-35.85)**(-2))
+    da		    = 6.11*(math.e**(17.269*(Ta - 273.15)/(Ta - 35.85))) - ea
+    delta	    = ((da + ea)*4097.9337)*((Ta - 35.85)**(-2))
     rcpg	    = rcp/gamma
     rcpgd	    = rcpg*delta
 
@@ -49,35 +49,35 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
     d		    = 0.66*zf
     zom 	    = max(zoms,0.13*zf)
     # soil-air aerodynamic resistance/conductance
-    xkzf	    = 0.4*0.4*ua*(zf-d)/math.log((za-d)/zom)
-    ras		    = zf*(math.e**xn)*((math.e**(-xn*zoms/zf))-(math.e**((-xn*(d+zom))/zf)))/(xn*xkzf)
+    xkzf	    = 0.4*0.4*ua*(zf - d)/math.log((za - d)/zom)
+    ras		    = zf*(math.e**xn)*((math.e**(-xn*zoms/zf)) - (math.e**((-xn*(d+zom))/zf)))/(xn*xkzf)
     # leaf-air aerodynamic resistance/conductance
-    uzf		    = ua*math.log((zf-d)/zom)/math.log((za-d)/zom)
-    rav		    = xn*math.sqrt(wl/uzf)/(4*alfo*glai*(1-math.e**(-xn/2)))
+    uzf		    = ua*math.log((zf - d)/zom)/math.log((za - d)/zom)
+    rav		    = xn*math.sqrt(wl/uzf)/(4*alfo*glai*(1 - math.e**(-xn/2)))
     # leaf-air stomatal conductance
     f 		    = (0.0055*max(rg,10)*2)/glai
-    frg 	    = (1+f)/(f+rstmin/5000)
+    frg 	    = (1 + f)/(f+rstmin/5000)
     fea 	    = 1 + 0.04*da
     # aerodynamic resistance in neutral conditions
-    alg 	    = math.log((za-d)/zom)
+    alg 	    = math.log((za - d)/zom)
     ra0 	    = alg*alg/(0.4*0.4*ua)
 
     # radiation forcing terms - layer/series approach
     [albs,ans,bns,cns,anv,bnv,cnv,cnas,cnav] = calcRn(rg,ratm,emiss,emisv,albe,albv,fcov,fcovl,albsmn,albsmx,albmode)
-    arns 	    = (ans*sigma*4*(Ta**3))*(1-xg)
+    arns 	    = (ans*sigma*4*(Ta**3))*(1 - xg)
     aras 	    = ans*sigma*4*(Ta**3)
-    brns 	    = (bns*sigma*4*(Ta**3))*(1-xg)
+    brns 	    = (bns*sigma*4*(Ta**3))*(1 - xg)
     bras 	    = bns*sigma*4*(Ta**3)
     arnv 	    = anv*sigma*4*(Ta**3)
     arav 	    = arnv
     brnv 	    = bnv*sigma*4*(Ta**3)
     brav 	    = brnv
-    crns 	    = ((ans+bns)*sigma*(Ta**4) + cns)*(1-xg)
-    crnv 	    = (anv+bnv)*sigma*(Ta**4) + cnv
-    cras 	    = (ans+bns)*sigma*(Ta**4) + cnas
-    crav 	    = (anv+bnv)*sigma*(Ta**4) + cnav
+    crns 	    = ((ans + bns)*sigma*(Ta**4) + cns)*(1 - xg)
+    crnv 	    = (anv + bnv)*sigma*(Ta**4) + cnv
+    cras 	    = (ans + bns)*sigma*(Ta**4) + cnas
+    crav 	    = (anv + bnv)*sigma*(Ta**4) + cnav
 
-    Mrad 	    = (1-emissf)*ratm + emissf*sigma*Tsurf**4
+    Mrad 	    = (1 - emissf)*ratm + emissf*sigma*Tsurf**4
 
     # initialization
     X0 		    = 5; X0old = X0
@@ -91,7 +91,7 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
         
         # air-air aerodynamic conductance
 	# Richardson Number
-        ri 	    = 5*(za-d)*9.81*X0/(ua*ua*Ta)
+        ri 	    = 5*(za - d)*9.81*X0/(ua*ua*Ta)
         if rg < 100:
             ri      = 0
 
@@ -100,7 +100,7 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
         else:
             p       = 2                                                                                 # stable conditions
 
-        ra 	    = ra0/((1+ri)**p)
+        ra 	    = ra0/((1 + ri)**p)
         ga 	    = 1/ra
 	# aggregated conductances for series/layer approach
         gav 	    = 1/rav
@@ -121,7 +121,7 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
 	# linking observed Tsurf with the source temperatures
         A3_1 	    = 0
         A3_2 	    = -aras - arav
-        A3_3 	    = -bras-brav
+        A3_3 	    = -bras - brav
 	    # RHS
         B1 	    = crns
         B2 	    = crnv - rcpg*gvv*ga*da/(gvv+ga)
@@ -189,12 +189,12 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
     # end stability loop
 
     # other SEB outputs - fluxes and temperatures
-    rns 	    = (crns + arns*X2 + brns*X3)/(1-xg)
+    rns 	    = (crns + arns*X2 + brns*X3)/(1 - xg)
     rnv 	    = (crnv + arnv*X2 + brnv*X3)
     rn 		    = rns + rnv
 
-    Hs 		    = rcp*gas*(X2-X0) 									# soil sensible heat
-    Hv 		    = rcp*gav*(X3-X0) 									# vegetation sensible heat
+    Hs 		    = rcp*gas*(X2 - X0) 								# soil sensible heat
+    Hv 		    = rcp*gav*(X3 - X0) 								# vegetation sensible heat
     H 		    = Hs + Hv 										# overall sensible heat flux
     LE		    = LEs + LEv										# overall latent heat energy
     G 		    = xg*rns
@@ -202,7 +202,7 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
     T0 		    = X0 + Ta 										# aerodynamic temperature in [K]
     Ts 		    = X2 + Ta 										# soil temperature in [K]
     Tv 		    = X3 + Ta 										# vegetation temperature in [K]
-    Tsf 	    = ((emissf*ratm - cras - crav - X2*(aras+arav) - X3*(bras+brav))/(sigma*emissf))**0.25
+    Tsf 	    = ((emissf*ratm - cras - crav - X2*(aras + arav) - X3*(bras + brav))/(sigma*emissf))**0.25
 
     return [LE,H,rn,G,LEv,LEs,Hv,Hs,Tv,Ts,Tsf]
 
@@ -210,24 +210,24 @@ def SPARSE(Tsurf,vza,rg,Ta,rh,ua,za,lai,glai,zf,rstmin,albv,emisv,emiss,emissf,a
 def calcRn(rg,ratm,emiss,emisv,albe,albv,fcov,fcovl,albsmn,albsmx,albmode):
     # calculate rn forcing terms
     if albmode      != 'Capped':
-        albs        = (albe-fcov*albv)/((1-fcov)**2 + fcov*albv*albe - (fcov*albv)**2)
+        albs        = (albe - fcov*albv)/((1 - fcov)**2 + fcov*albv*albe - (fcov*albv)**2)
     else:
-        albs 	    = max(albsmn,min(albsmn,(albe-fcov*albv)/((1-fcov)**2 + fcov*albv*albe - (fcov*albv)**2)))		
+        albs 	    = max(albsmn,min(albsmn,(albe - fcov*albv)/((1 - fcov)**2 + fcov*albv*albe - (fcov*albv)**2)))		
 
-    # albs 	    = (albe-fcov*albv)/((1-fcov)**2 + fcov*albv*albe - (fcov*albv)**2)
-    # albs 	    = max(albsmn,min(albsmn,(albe-fcov*albv)/((1-fcov)**2 + fcov*albv*albe - (fcov*albv)**2)))
+    # albs 	    = (albe - fcov*albv)/((1 - fcov)**2 + fcov*albv*albe - (fcov*albv)**2)
+    # albs 	    = max(albsmn,min(albsmn,(albe - fcov*albv)/((1 - fcov)**2 + fcov*albv*albe - (fcov*albv)**2)))
     v1              = 1 - albv*albs*fcov; 	v1a = 1 - albv*albs*fcovl
     v2 	            = 1 - emisv
     v3 	            = 1 - emiss
-    v4 		    = 1 - fcov;                     v4a = 1 - fcovl
+    v4 		    = 1 - fcov;                 v4a = 1 - fcovl
     v5 		    = 1 - fcov*v2*v3; 		v5a = 1 - fcovl*v2*v3
     arns 	    = -(v4a*emiss + emisv*emiss*fcovl)/v5a
     brns 	    = (emisv*emiss*fcovl)/v5a
-    crns 	    = (rg*(1-albs)*v4)/v1 + (v4a*emiss*ratm)/v5a
+    crns 	    = (rg*(1 - albs)*v4)/v1 + (v4a*emiss*ratm)/v5a
     cras 	    = (v4a*emiss*ratm)/v5a
     arnv 	    = brns
     brnv 	    = -fcovl*(emisv + (emisv*emiss + v4a*v3*emisv)/v5a)
-    crnv 	    = rg*(1-albv)*fcov*(1 + (albs*v4)/v1) + fcovl*emisv*ratm*(1 + (v4a*v3)/v5a)
+    crnv 	    = rg*(1 - albv)*fcov*(1 + (albs*v4)/v1) + fcovl*emisv*ratm*(1 + (v4a*v3)/v5a)
     crav 	    = fcovl*emisv*ratm*(1 + (v4a*v3)/v5a)
 
     return [albs,arns,brns,crns,arnv,brnv,crnv,cras,crav]
@@ -235,7 +235,7 @@ def calcRn(rg,ratm,emiss,emisv,albe,albv,fcov,fcovl,albsmn,albsmx,albmode):
 def SEBsoln(A1_1,A1_2,A1_3,A2_1,A2_2,A2_3,A3_1,A3_2,A3_3,B1,B2,B3):
     # SEB matrix solution [A|B] ; [X]=Inv[A][B]
         # determinant of A
-    detA 	    = A1_1*(A2_2*A3_3-A2_3*A3_2) - A1_2*(A2_1*A3_3-A2_3*A3_1) + A1_3*(A2_1*A3_2-A2_2*A3_1)
+    detA 	    = A1_1*(A2_2*A3_3 - A2_3*A3_2) - A1_2*(A2_1*A3_3 - A2_3*A3_1) + A1_3*(A2_1*A3_2 - A2_2*A3_1)
         # inverse matrix coefficients
     IA1_1	    =  (A2_2*A3_3 - A2_3*A3_2)/detA
     IA1_2	    = -(A1_2*A3_3 - A1_3*A3_2)/detA
